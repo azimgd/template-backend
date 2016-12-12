fc3113632cfb3cde0dc752a275110687c9f42c9a
Azim Gadzhiagayev <me@azimgd.com>
2016-12-12 13:23:11 +0500

    Fix heroku + sequelize support

--- src/db.js
+++ src/db.js
@@ -1,8 +1,23 @@
+import path from 'path';
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 import Sequelize from 'sequelize';
+import url from 'url';
+
+const getCredentials = (clearDb) => {
+	const urlObject = url.parse(clearDb);
+	const auth = urlObject.auth.split(':');
+	return {
+		host: urlObject.host,
+		username: auth[0],
+		password: auth[1],
+		name: urlObject.pathname.substring(1),
+	};
+}
+
+const credentials = getCredentials(process.env.CLEARDB_DATABASE_URL);
 
 export default callback => {
-	const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
-		host: process.env.DATABASE_HOST,
+	const sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL, {
 		dialect: 'mysql',
 
 		pool: {
