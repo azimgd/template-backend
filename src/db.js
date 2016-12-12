39c6a819c0bd05c1db5f83c3826c1a5b638b032d
Azim Gadzhiagayev <me@azimgd.com>
2016-12-12 11:10:24 +0500

    Move credentials to env vars

--- src/db.js
+++ src/db.js
@@ -1,8 +1,8 @@
 import Sequelize from 'sequelize';
 
 export default callback => {
-	const sequelize = new Sequelize('shop', 'root', '', {
-		host: 'localhost',
+	const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
+		host: process.env.DATABASE_HOST,
 		dialect: 'mysql',
 
 		pool: {
